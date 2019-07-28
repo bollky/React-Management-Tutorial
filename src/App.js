6c923f8fc46b6ecd1ff167f9c936860695816851
Jung O Chun <jungoc@banyule.vic.gov.au>
2019-07-28 20:21:20 +1000

    Create customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
 
+const customers = [
+  {
+    'id':1,
+    'image':'https://placeimg.com/64/64/1',
+    'name': '천정오',
+    'birthday': '740215',
+    'gender': '남자',
+    'job':'직장인'
+  },
+  {
+    'id':2,
+    'image':'https://placeimg.com/64/64/2',
+    'name': '홍길동',
+    'birthday': '731205',
+    'gender': '남자',
+    'job':'실업자'
+  },
+  {
+    'id':3,
+    'image':'https://placeimg.com/64/64/3',
+    'name': '고문관',
+    'birthday': '750111',
+    'gender': '남자',
+    'job':'교수'
+  }
+]
 class App extends Component {
   render(){
     return( 
-      <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>let's develop management system!</h2>
-      </div>
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>      
     );
   }
 }
